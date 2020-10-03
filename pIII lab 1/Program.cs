b38874a3bcd72d2ef2c0b5b18c507d36525c8e82
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2020-10-03 12:31:36 +0200

    Update Program.cs

--- pIII lab 1/Program.cs
+++ pIII lab 1/Program.cs
@@ -4,9 +4,31 @@ namespace pIII_lab_1
 {
     class Program
     {
+        static void Funkcja(ref int liczba)
+        {
+            Console.WriteLine("F: "+liczba);
+            liczba = 200;
+            Console.WriteLine("F: " + liczba);
+        }
+        static void Funkcja(ref int liczba)
+        {
+            Console.WriteLine("F: "+tablica[0]);
+            tablica[0] = 200;
+            Console.WriteLine("F: " + tablica[0]);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int zmienna = 100;
+            float liczbazmiennoprzecinkowa1 = 2.5f;
+            double liczbazmiennoprzecinkowa2 = 25;
+            char litera = 'h';
+            string slowo = "h";
+            int[]_tablica = [1, 2, 3];
+
+            Console.WriteLine("M")
+
+
         }
+
     }
 }
